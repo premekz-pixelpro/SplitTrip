77a68a152e5c69db3fb65672a396a27a7ba3a14d
premekz-pixelpro <office@pixelpro.pl>
2025-08-11 13:39:03 +0200

    feat: update development server configuration and add TanStack Router
    - Changed the development server port from 3000 to 80 in package.json and vite.config.ts.
    - Added TanStack Router and its devtools as dependencies in package.json.
    - Integrated TanStack Router plugin in vite.config.ts with auto code splitting enabled.
    - Updated HMR configuration to use port 80 and switched protocol to 'ws'.
    - Added alias for hooks in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,34 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
-import fs from 'fs';
-
+// import fs from 'fs';
 
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react(),
     tailwindcss(),
   ],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'home.pixelpro.pl-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'home.pixelpro.pl.pem')),
-    },
-    host: 'home.pixelpro.pl',
-    port: 3000, // lub inny, jeśli potrzebujesz
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, 'home.pixelpro.pl-key.pem')),
+    //   cert: fs.readFileSync(path.resolve(__dirname, 'home.pixelpro.pl.pem')),
+    // },
+    host: true,
+    port: 80,
     hmr: {
       host: 'home.pixelpro.pl',
-      port: 3000,
-      protocol: 'wss'
-    }
+      port: 80,
+      protocol: 'ws',
+    },
   },
-  
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -32,6 +36,7 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@store': path.resolve(__dirname, 'src/store'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
     },
   },
-})
+});
